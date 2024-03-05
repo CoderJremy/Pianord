bdcb062d2945f88434127e662a66596a437db6a7
CoderJremy <jeremy66frias@gmail.com>
2024-03-04 21:21:29 -0500

    Basic Foundation Set For The Logic

--- main.js
+++ main.js
@@ -1,69 +1,112 @@
 const piano = document.getElementById("piano");
-const allNotes = ["C", "C#", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B"];
+const allWhiteNotes = ["C", "D", "E", "F", "G", "A", "B"];
+const allBlackNotes = ["C#", "Eb", "F#", "Ab", "Bb"];
+const everyNote = allWhiteNotes.concat(allBlackNotes);
+const keyboardChars = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0",
+    "-",
+    "=",
+    "a",
+    "w",
+    "s",
+    "e",
+    "d",
+    "f",
+    "t",
+    "g",
+    "y",
+    "h",
+    "u",
+    "j",
+    "k",
+    "l",
+    ";",
+    "'",
+    "z",
+    "x",
+    "c",
+    "v",
+    "b",
+    "n",
+    "m",
+    ",",
+];
 
 const createBoard = (octaves) => {
     let allKeys = [];
-    function createKey(text, clsName, parent) {
+
+    function createKey(clsName, parent) {
         const key = document.createElement("div");
         key.className = clsName;
-        key.innerText = text;
+        key.classList.add("key");
         allKeys.push(key);
         parent.appendChild(key);
     }
 
-    let octaveSection;
     function createOctaves(amount, groupOfKeys) {
         for (let i = 0; i < amount; i++) {
-            octaveSection = document.createElement("section");
-            octaveSection.className = "octave";
+            const octaveDisplay = document.createElement("section");
+            octaveDisplay.className = "octave";
+
+            // Create black and white key groups within each octave
+            const whiteGroupDisplay = document.createElement("div");
+            whiteGroupDisplay.className = "white-keys-only";
+            octaveDisplay.appendChild(whiteGroupDisplay);
+
+            const blackGroupDisplay = document.createElement("div");
+            blackGroupDisplay.className = "black-keys-only";
+            octaveDisplay.appendChild(blackGroupDisplay);
 
             groupOfKeys.map((note) => {
                 switch (note) {
                     case "C":
-                        createKey(note, "white key", octaveSection);
+                    case "D":
+                    case "E":
+                    case "F":
+                    case "G":
+                    case "A":
+                    case "B":
+                        createKey("white " + note.toLowerCase() + "-note", whiteGroupDisplay);
                         break;
                     case "C#":
-                        createKey(note, "black key", octaveSection);
-                        break;
-                    case "D":
-                        createKey(note, "white key", octaveSection);
+                        createKey("black cs-note", blackGroupDisplay);
                         break;
                     case "Eb":
-                        createKey(note, "black key", octaveSection);
-                        break;
-                    case "E":
-                        createKey(note, "white key", octaveSection);
-                        break;
-                    case "F":
-                        createKey(note, "white key", octaveSection);
+                        createKey("black ef-note", blackGroupDisplay);
                         break;
                     case "F#":
-                        createKey(note, "black key", octaveSection);
-                        break;
-                    case "G":
-                        createKey(note, "white key", octaveSection);
+                        createKey("black fs-note", blackGroupDisplay);
                         break;
                     case "Ab":
-                        createKey(note, "black key", octaveSection);
-                        break;
-                    case "A":
-                        createKey(note, "white key", octaveSection);
+                        createKey("black af-note", blackGroupDisplay);
                         break;
                     case "Bb":
-                        createKey(note, "black key", octaveSection);
-                        break;
-                    case "B":
-                        createKey(note, "white key", octaveSection);
+                        createKey("black bf-note", blackGroupDisplay);
                         break;
                 }
             });
 
-            piano.appendChild(octaveSection);
+            piano.appendChild(octaveDisplay);
         }
     }
 
-    createOctaves(octaves, allNotes);
+    createOctaves(octaves, everyNote);
     console.log(allKeys);
+
+    allKeys.forEach((key) => {
+        key.addEventListener("click", (e) => {
+            console.log(e.target.className);
+        });
+    });
 };
 
-createBoard(2);
+createBoard(3);
